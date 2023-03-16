cc2454112c4cc5bc985e818edb79bbd5107fd269
Leona <leonasyla3@gmail.com>
2023-03-16 17:23:56 +0100

    Adding project dto

---
+++ src/api/project/dtos/create-project.dto.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
+import { ProjectType } from '../enums/types.enum';
+
+export class CreateProjectDto {
+  @IsString()
+  @ApiProperty()
+  url: string;
+
+  @IsString()
+  @ApiProperty()
+  name: string;
+
+  @IsEnum(ProjectType)
+  @ApiProperty()
+  type: ProjectType;
+}
