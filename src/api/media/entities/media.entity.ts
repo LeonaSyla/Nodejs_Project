43416788d8befbd6663835b6ce9c3ba7e1c59960
Leona <leonasyla3@gmail.com>
2023-03-23 11:39:09 +0100

    done: Add media module with entity and enum

--- src/api/media/entities/media.entity.ts
+++ src/api/media/entities/media.entity.ts
@@ -5,9 +5,7 @@ import { Entity, Column } from 'typeorm';
 
 @Entity()
 export class Media extends AuditEntity {
-  @Column({ primary: true, generated: true })
-  id: number;
-
+  
   @Column()
   key: string;
 
